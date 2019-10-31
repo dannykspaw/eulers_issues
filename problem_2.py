ad6813647f16e740bcdd744b9deeb78d3aa65b46
Hedrick9 <hedrickrussell@gmail.com>
2019-10-30 21:50:30 -0700

    Russell's Solution

--- problem_2.py
+++ problem_2.py
@@ -9,8 +9,21 @@
     whose values do not exceed four million, find the sum
     of the even-valued terms.
 '''
+n = 1
+x = [1, 2]
+i = 0
+while n < 4000000:
+    x.append(x[i] + x[i+1])
+    i = i + 1
+    n = x[i+1]
+sum = 0
+for i in range(len(x)):
+    if x[i] % 2 == 0:
+        sum = sum + x[i]
 
-x = 1
-y = 1
-for i in range(10):
-    print("sup" + i)
+print(f'''
+         {x}
+         {i}
+         {n}
+         {sum}
+       ''')
