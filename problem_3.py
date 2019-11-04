1c1e1a8efe52bbf75be84ba25fcd83b614d9476b
Russell <hedrickrussell@gmail.com>
2019-11-03 20:55:30 -0800

    Current progress for problem 3

---
+++ problem_3.py
@@ -0,0 +1,23 @@
+import math
+
+def prime_finder(n):
+    if n == 1:
+        return False
+    if n == 2: 
+        return True
+    if n % 2 == 0:
+        return False
+    divisor = math.floor(math.sqrt(n))
+    if n > 2:
+        for i in range(3, 1 + divisor, 2):
+            if n % i == 0:
+                return False
+        return True
+print(prime_finder(104729))
+
+x = int(input("User input number:\n"))
+for i in range(x, 3, -2):
+    if prime_finder(i) == True:
+        print(i)
+        break
+print('The number is prime and has no prime factors.')
